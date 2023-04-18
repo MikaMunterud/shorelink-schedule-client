7796a8a4aea367e865046f64dacc50375798a0a2
Mika Munterud <mika.munterud@outlook.com>
2023-04-19 01:45:35 +0200

    Adding original worker context that contains all workers

---
+++ src/contexts/OriginalWorkerContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { GlobalContext } from "./GlobalContext";
+
+export const OriginalWorkerContext = createContext();
+
+export function OriginalWorkerProvider({ children }) {
+  const schedule = useContext(GlobalContext);
+
+  if (!schedule) {
+    return null;
+  }
+
+  const originalWorkers = schedule.map(function (worker) {
+    return worker.worker;
+  });
+
+  if (!originalWorkers) {
+    <section className="mainSection">
+      <h3>Laddar...</h3>
+    </section>;
+  }
+
+  return (
+    <OriginalWorkerContext.Provider value={originalWorkers}>
+      {children}
+    </OriginalWorkerContext.Provider>
+  );
+}
