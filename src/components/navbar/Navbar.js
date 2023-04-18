4895cfd4ce84ea646248117b561fb4477754af33
Mika Munterud <mika.munterud@outlook.com>
2023-04-19 01:43:17 +0200

    Adding navbar and styling

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import MenuList from "./MenuList";
+import MenuIcons from "./MenuIcons";
+import { useNavigate } from "react-router-dom";
+import "../../sass/navbar/Navbar.scss";
+import { MdOutlineSettings } from "react-icons/md";
+
+export default function Navbar() {
+  const [menuList, setMenuList] = useState("");
+  const [menu, setMenu] = useState(true);
+  const [cross, setCross] = useState(false);
+  const navigate = useNavigate();
+
+  function toggleMenu() {
+    if (menu) {
+      setMenu(false);
+      setCross(true);
+      setMenuList("visible");
+    } else {
+      hideMenu();
+    }
+  }
+
+  function hideMenu() {
+    setCross(false);
+    setMenu(true);
+    setMenuList("");
+
+    /*
+     * This closes all details lists in the menu when closing.
+     */
+    const details = document.querySelectorAll("details");
+    details.forEach(function (list) {
+      list.removeAttribute("open");
+    });
+  }
+
+  const goToHomepage = function () {
+    navigate("/");
+  };
+
+  function redirect(endpoint) {
+    navigate(`${endpoint}`);
+  }
+
+  return (
+    <header className="fixedHeader">
+      <div className="relativeHeader">
+        <div className="navIcons">
+          <nav className="navIcons_menu">
+            <MenuIcons toggleMenu={toggleMenu} menu={menu} cross={cross} />
+
+            <MenuList menuList={menuList} hideMenu={hideMenu} />
+          </nav>
+        </div>
+        <img
+          className="companyLogo"
+          src={"https://icicathy.sirv.com/shorelink_white.png"}
+          alt={"company logo"}
+          width={"400px"}
+          height={"auto"}
+          onClick={goToHomepage}
+        />
+        <div className="userSettingsIcon" onClick={hideMenu}>
+          <MdOutlineSettings
+            className="userSettingsIcon_icon"
+            onClick={function () {
+              redirect("/userSettings");
+            }}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
