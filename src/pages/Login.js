b0bf7312ab78dc5ae6ccfcf843af29fdaac6edf6
Mika Munterud <mika.munterud@outlook.com>
2023-04-19 10:28:46 +0200

    Updating path for message component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserForm from "../components/UserForm";
-import Message from "../components/Message";
+import Message from "../components/messages/Message";
 import { AuthenticationContext } from "../contexts/AuthenticationContext";
 
 export default function Login() {
