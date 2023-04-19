2994c128268fde94aecb6693357884187268690a
Mika Munterud <mika.munterud@outlook.com>
2023-04-20 00:04:31 +0200

    Adding id to not logged in component

--- src/components/messages/NotLoggedIn.js
+++ src/components/messages/NotLoggedIn.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotLoggedIn() {
   return (
-    <section className="mainSection">
+    <section className="mainSection" id="notLoggedInMessage">
       <h2>Vänligen logga in för att använda denna applikation!</h2>
       <h3>
         <Link to={"/"}>Tillbaka till inloggningssidan!</Link>
