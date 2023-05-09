047a90d26ba755392834c68c7d7e51b6fbe109c8
Mika Munterud <mika.munterud@outlook.com>
2023-05-09 20:28:07 +0200

    Adding message for cookies handling and server error

--- src/functions/wakeUpServer.js
+++ src/functions/wakeUpServer.js
@@ -1,10 +1,27 @@
+import Swal from "sweetalert2";
+
 export async function wakeUpServer() {
   try {
     await fetch(`${process.env.REACT_APP_BASE_URL}/ping`, {
       method: "GET",
       credentials: "include",
     });
+
+    if (!localStorage.getItem("firstTimeVisit")) {
+      return await Swal.fire(
+        "Denna sida använder cookies 🍪",
+        "För att använda sidans alla funktion se till att ändra din webbläsares inställningar till att tillåta cookies och spårning mellan webbplatser.",
+        "info"
+      );
+    }
+
+    return;
   } catch (FetchError) {
-    return "Går inte att koppla upp till server";
+    Swal.fire(
+      `Misslyckades att koppla upp till server, kom tillbaka senare!`,
+      "",
+      "error"
+    );
+    return;
   }
 }
